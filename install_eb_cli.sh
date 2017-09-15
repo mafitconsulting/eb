096671b9b44c4272bf373400e6b374ff69a36d21
root <root@fieldhousem1.mylabserver.com>
2017-09-15 09:10:38 +0000

    first cut

---
+++ install_eb_cli.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+yum install -y python3.4
+curl -O https://bootstrap.pypa.io/get-pip.py
+python get-pip.py --user
+cp ~/.bash_profile ~/.bash_profile.bk
+pip install awsebcli --upgrade --user
+
