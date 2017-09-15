245e8f0ca7484131d2ab5a30fa8c2709f0c86a89
root <root@fieldhousem1.mylabserver.com>
2017-09-15 10:40:19 +0000

    updated with repos

--- install_eb_cli.sh
+++ install_eb_cli.sh
@@ -4,4 +4,6 @@ curl -O https://bootstrap.pypa.io/get-pip.py
 python get-pip.py --user
 cp ~/.bash_profile ~/.bash_profile.bk
 pip install awsebcli --upgrade --user
+yum install -y docker && yum install -y awscli
+service docker start
 
